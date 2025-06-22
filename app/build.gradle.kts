a24536d82a2e52f5cb890aebeef06d395623d81d
wangkang1995 <1226426603@qq.com>
2025-06-22 20:53:23 +0800

    sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "cn.wk.android.sync.hub"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "cn.wk.android.sync.hub"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
